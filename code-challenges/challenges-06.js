908c65653abbe09dfb1e88ea692bea5f2a6e7cb4
yazan2181993 <yazanshhab123@gmail.com>
2023-02-09 02:20:49 +0000

    challenge done

---
+++ code-challenges/challenges-06.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 01:
+//  Required:
+//
+//  Write a function that takes an array and returns the maximum value in the provided array
+//  without using build-in functions.
+//  
+// Input: [12, 32, 22, 45, 78, 12, 50]
+// Output: 78
+//  
+
+const findMax = (arr)=>{
+    let max = arr[0];
+    // write your code here
+    for(let i=0;i<=arr.length-1;i++){
+        if (max < arr[i]) {
+            max = arr[i]
+        }
+    }
+    return max;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 02:
+//  Required:
+//
+//  Write a function that takes an arrays and returns the Sum of the numeric values
+//  inside the array (there could be strings inside), without using built-in functions
+//  
+// Input: [20, '234', 'car', 41, 20, 'chair']
+// Output: 81
+//  
+
+const sumNums = (arr)=>{
+    let sum = 0;
+    // write your code here
+    for(let i=0;i<arr.length;i++){
+        sum === sum + arr[i];
+    }
+    return sum;
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 03:
+//  optional:
+//
+//  Write a function that takes an array and returns the array reversed
+//  without using built-in functions
+//  
+// Input: ['C#', 'JS', 'Ruby','Python'] 
+// Output: ['Python','Ruby','JS','C#']
+
+const reverseArray = (arr)=>{
+    // write your code here
+    let reverseArray = arr.reverse()
+return reverseArray;
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = {findMax , sumNums, reverseArray};
